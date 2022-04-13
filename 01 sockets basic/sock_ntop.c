ae2289eb04ff93ce5f29a9c5621a2a88cbd7e5d9
HunterPlus <zlw08140957@gmail.com>
2022-04-13 15:03:53 +0800

    Update sock_ntop.c

--- 01 sockets basic/sock_ntop.c
+++ 01 sockets basic/sock_ntop.c
@@ -12,6 +12,7 @@ char *sock_ntop(const struct sockaddr *sa, socklen_t salen)
 	switch (sa->sa_family) {
 	case AF_INET: {
 		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+		
 		if (inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
 			return NULL;
 		if ((port = ntohs(sin->sin_port)) != 0) {
@@ -21,6 +22,17 @@ char *sock_ntop(const struct sockaddr *sa, socklen_t salen)
 		return str;
 	}
 	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6) sa6;
+		
+		str[0] = '[';
+		if (inet_ntop(AF_INET6, &sin6->sin6_addr, str + 1, sizeof(str) - 1) == NULL)
+			return NULL;
+		if ((port = ntohs(sin6->sin6_port)) != 0) {
+			snprintf(portstr, sizeof(portstr), "]:%d", port);
+			strcat(str, portstr);
+			return str;
+		}
+		return str + 1;
 	}
 	default:
 		snprintf(str, sizeof(str), "sock_ntop: unknown AFxxx: %d, len %d", sa->sa_family, salen);
