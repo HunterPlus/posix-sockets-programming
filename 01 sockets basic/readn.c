eccc8c832b9c247d487485e75f5a44c054b8dd7b
HunterPlus <zlw08140957@gmail.com>
2022-04-13 18:02:12 +0800

    readn

---
+++ 01 sockets basic/readn.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <errno.h>
+
+size_t readn(int fd, void *buff, size_t n)
+{
+        size_t  nleft;
+        ssize_t nread;
+        char    *ptr;
+
+        ptr = buff;
+        nleft = n;
+        while (nleft > 0) {
+                if ((nread = read(fd, ptr, nleft)) < 0) {
+                        if (errno == EINTR)
+                                nread = 0;      /* and call read() again */
+                        else
+                                return (-1);
+                } else if (nread == 0)
+                        break;          /* EOF */
+                
+                nleft -= nread;
+                ptr += nread; 
+        }
+        return (n - nleft);
+}
