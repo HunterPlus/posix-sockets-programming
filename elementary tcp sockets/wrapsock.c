f91c55d07a399f35064ed35837a8a4867550c2be
HunterPlus <zlw08140957@gmail.com>
2022-04-14 13:44:53 +0800

    Create wrapsock.c

---
+++ elementary tcp sockets/wrapsock.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+void Listen(int fd, int backlog)
+{
+	char	*ptr;
+	
+	if ((ptr = getenv("LISTENQ")) != NULL)
+		backlog = atoi(ptr);
+	
+	if (listen(fd, backlog) == -1) {			
+	}
+}
