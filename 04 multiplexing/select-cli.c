c15076193ccd8cf1ad39c4af33a8e8f61800e039
HunterPlus <zlw08140957@gmail.com>
2022-04-18 20:47:04 +0800

    Update select-cli.c

--- 04 multiplexing/select-cli.c
+++ 04 multiplexing/select-cli.c
@@ -6,4 +6,25 @@
 void str_cli(FILE *fp, int sockfd);
 int main(int argc, char *argv[])
 {
+	int	sockfd;
+	struct sockaddr_in servaddr;
+	
+	if (argc != 2)
+		err_quit("usage: selct_cli <IPaddress>");
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr.s_addr);
+	
+	Connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	
+	str_cli(stdin, sockfd);
+	
+	exit(0);
+}
+
+void str_cli(FILE *fp, int sockfd)
+{
+	
 }
