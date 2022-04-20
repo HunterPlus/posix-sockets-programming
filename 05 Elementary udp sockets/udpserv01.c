c30d0a544e8b8703be968bb833c9b5ddd3709601
HunterPlus <zlw08140957@gmail.com>
2022-04-20 16:10:54 +0800

    Update udpserv01.c

--- 05 Elementary udp sockets/udpserv01.c
+++ 05 Elementary udp sockets/udpserv01.c
@@ -2,7 +2,34 @@
 
 #define SERV_PORT	2022
 
+void dg_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen);
 int main(int argc, char *argv[])
 {
 	int	sockfd;
+	struct sockaddr_in servaddr, cliaddr;
+	
+	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+	
+	Bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	
+	dg_echo(sockfd, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
+}
+
+void dg_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen)
+{
+	int	n;
+	socklen_t len;
+	char	mesg[MAXLINE];
+	
+	while (1) {
+		len = clilen;
+		n = Recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+		
+		Sendto(sockfd, mesg, n, 0, pcliaddr, len);
+	}
 }
